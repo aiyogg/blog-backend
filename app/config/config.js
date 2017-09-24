b8567f25a915357adb97fa463871c1775bb66f4a
Chuck <tengchenc@gmail.com>
2017-09-24 17:54:34 +0800

    configure local database address

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongodb: "mongodb://localhost:27017/blog",
+  mongodb: "mongodb://localhost:27017/myblog",
   baiduAK:"yFKaMEQnAYc1hA0AKaNyHGd4HTQgTNvO",
   //mongodb: "mongodb://admin:f642sgf5et@localhost:27017/admin"//err psw! notice!!!!
 };
