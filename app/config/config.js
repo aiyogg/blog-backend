78edca5d24807a88b4472ff205a85cf6a7b6be48
Chuck <tengchenc@gmail.com>
2017-12-14 00:10:08 +0800

    add option mongodb connect api

--- app/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mongodb: "mongodb://localhost:27017/myblog",
-  baiduAK:"yFKaMEQnAYc1hA0AKaNyHGd4HTQgTNvO",
-  //mongodb: "mongodb://admin:f642sgf5et@localhost:27017/admin"//err psw! notice!!!!
-  upyunBucketPath: 'http://v0.api.upyun.com/xxxx/uploads/',
-  upyunVisitUrl: 'https://xxxx.b0.upaiyun.com/uploads/',
-  upyunOperator: 'admin',
-  upyunPwd: 'xxxxx' // error pwd!
-};
