3dffe10f69fd222178c84a866e3759307e86a245
chuck <tengchenc@gmail.com>
2021-06-19 15:22:47 +0800

    feat: add example config file

---
+++ app/config/config.EXAMPLE.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mongodb: 'mongodb://USERNAME:PASSWORD@localhost:27017/blog',
+  baiduAK: 'BD_AK',
+  upyunBucketPath: 'http://v0.api.upyun.com/BUCKET/uploads/',
+  upyunVisitUrl: 'https://cdn.mydomain.com/uploads/',
+  upyunOperator: 'admin',
+  upyunPwd: 'upyun@2017',
+  wbMyToken: 'WB_TOKEN',
+  wbAppKey: 'WB_KEY',
+  wbAppSecret: 'WB_SECRET',
+  wbRedirectUrl: 'https://blog.mydomain.com/oauth/wb',
+}
